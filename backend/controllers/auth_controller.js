7c7632861354393ca31edd98212d91dd77acfd5a
Akshat Bhardwaj <akshatbhardwaj019@gmail.com>
2024-06-09 17:06:03 +0530

    3-auth handled and routes protected in client

--- backend/controllers/auth_controller.js
+++ backend/controllers/auth_controller.js
@@ -8,21 +8,21 @@ export const signup = async (req,res) =>{
      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
      if(!emailRegex.test(email)){
-        res.status(400).json({error: "Invalid email format"});
+       return  res.status(400).json({error: "Invalid email format"});
      }
      
      const existingUser = await User.findOne({username});
      if(existingUser){
-        res.status(400).json({error: "Username already exists"});
+        return res.status(400).json({error: "Username already exists"});
      }
 
      const existingEmail = await User.findOne({email});
      if(existingEmail){
-        res.status(400).json({error: "Email is already taken"});
+        return res.status(400).json({error: "Email is already taken"});
      }
 
      if(password.length<6){
-        res.status(400).json({error: "Password should be of atleast 6 characters"});
+        return res.status(400).json({error: "Password should be of atleast 6 characters"});
      }
      
      const salt = await bcrypt.genSalt(10);
@@ -39,7 +39,7 @@ export const signup = async (req,res) =>{
         generateTokenAndSetCookie(newUser._id,res);  
         await newUser.save();
 
-        res.status(201).json({
+        return res.status(201).json({
             _id: newUser._id,
             fullName: newUser.fullName,
             email: newUser.email,
@@ -51,11 +51,11 @@ export const signup = async (req,res) =>{
         })
      }
      else{
-        res.status(400).json({error: "Invalid user data"});
+        return res.status(400).json({error: "Invalid user data"});
      }
    } catch (error) {
       console.log(`Error in signup controller: ${error.message}`);
-      res.status(500).json({error: "Error at server"});
+      return res.status(500).json({error: "Error at server"});
    }
 }
 
@@ -66,11 +66,11 @@ export const login = async (req,res) =>{
         const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
 
         if(!user || !isPasswordCorrect)
-           res.status(400).json({error: "Invalid username or password"});  
+           return res.status(400).json({error: "Invalid username or password"});  
         
         generateTokenAndSetCookie(user._id,res);
 
-        res.status(200).json({
+        return res.status(200).json({
             _id: user._id,
             fullName: user.fullName,
             email: user.email,
@@ -82,26 +82,26 @@ export const login = async (req,res) =>{
         });
     } catch (error) {
         console.log(`Error in login controller: ${error.message}`);
-        res.status(500).json({error: "Error at server"});
+        return res.status(500).json({error: "Error at server"});
     }
 }
 
 export const logout = async (req,res) =>{
     try {
         res.cookie("jwt","",{maxAge:0})
-        res.status(200).json({meassage: "Logged out successfully"});
+        return res.status(200).json({meassage: "Logged out successfully"});
     } catch (error) {
         console.log(`Error in logout controller: ${error.message}`);
-        res.status(500).json({error: "Error at server"});
+        return res.status(500).json({error: "Error at server"});
     }
 }
 
 export const getMe = async (req,res) =>{
    try {
       const user = await User.findById(req.user._id).select("-password");
-      res.status(200).json(user);
+      return res.status(200).json(user);
    } catch (error) {
       console.log(`Error in getMe controller: ${error.message}`);
-      res.status(500).json({error: "Error at server"});
+      return res.status(500).json({error: "Error at server"});
    }
 }
--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -3,14 +3,38 @@ import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const Sidebar = () => {
-	const data = {
-		fullName: "John Doe",
-		username: "johndoe",
-		profileImg: "/avatars/boy1.png",
-	};
+	
+	const queryClient = useQueryClient();
 
+	const {mutate: logout} = useMutation({
+        mutationFn: async() => {
+			try {
+				const res = await fetch("/api/auth/logout",{
+					method: "POST",
+				});
+
+				const data = await res.json();
+				if(!res.ok) { throw new Error(data.error || "Something went wrong")}
+
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess : () => {
+			// toast.success("Logout successful");
+            queryClient.invalidateQueries({queryKey: ['authUser']});
+		},
+		onError : () => {
+			toast.error("Logout failed");
+		}
+	});
+
+	const {data} = useQuery({queryKey:['authUser']});
+	
 	return (
 		<div className='md:flex-[2_2_0] w-18 max-w-52'>
 			<div className='sticky top-0 left-0 h-screen flex flex-col border-r border-gray-700 w-20 md:w-full'>
@@ -62,7 +86,13 @@ const Sidebar = () => {
 								<p className='text-white font-bold text-sm w-20 truncate'>{data?.fullName}</p>
 								<p className='text-slate-500 text-sm'>@{data?.username}</p>
 							</div>
-							<BiLogOut className='w-5 h-5 cursor-pointer' />
+							<BiLogOut className='w-5 h-5 cursor-pointer' 
+								onClick={(e) => {
+									e.preventDefault();
+									console.log("press")
+									logout();
+								}}
+							/>
 						</div>
 					</Link>
 				)}
