15b53b21647d014a8335371c7d24838ed591899d
Akshat Bhardwaj <akshatbhardwaj019@gmail.com>
2024-06-11 18:45:03 +0530

    3- create posts added

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(express.json());
+app.use(express.json({limit: "5mb"}));
 app.use(express.urlencoded({extended: true}));
 
 app.use(cookieParser());
