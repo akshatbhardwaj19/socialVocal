f5b3666aae446dec20bba45591bfadc4aede8d68
Akshat Bhardwaj <akshatbhardwaj019@gmail.com>
2024-06-12 15:30:33 +0530

    3- like post added

--- backend/controllers/post_controller.js
+++ backend/controllers/post_controller.js
@@ -92,7 +92,9 @@ export const likeUnlikePost = async(req,res) =>{
       if(isAlreadyLiked){
          await Post.updateOne({_id:postId},{$pull: {likes: userId}});
          await User.updateOne({_id:userId},{$pull: {likedPosts: postId}});
-         res.status(200).json({message: "Post unliked successfully"});   
+
+         const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString());
+         res.status(200).json(updatedLikes);   
       }
       else{
         post.likes.push(userId);
@@ -104,9 +106,10 @@ export const likeUnlikePost = async(req,res) =>{
             to: post.user,
             type: "like"
         });
-
+       
         await notification.save();
-        res.status(200).json({message: "Post liked successfully"});
+        const updatedLikes = post.likes;
+        res.status(200).json(updatedLikes);
       }
    } catch (error) {
        console.log(`Error in likeUnlikePost controller: ${error.message}`);
